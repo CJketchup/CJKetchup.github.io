bf07e282c95e237c2ec7edab14580714ca45eef0
CJketchup <jchiao6900@gmail.com>
2025-05-06 04:08:45 +0800

    Add files via upload

---
+++ mapping.js
@@ -0,0 +1,3 @@
+const countyMapping = JSON.parse(document.getElementById("countyJSON").textContent);
+const addressMapping = JSON.parse(document.getElementById("addressJSON").textContent);
+const streetMapping = JSON.parse(document.getElementById("streetJSON").textContent);
